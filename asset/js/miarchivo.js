60e0f349d9288f6213e3f96edf9be402064c14cf
santuyi <benitezsantiago18@gmail.com>
2023-03-14 23:29:15 -0300

    agregando java script

---
+++ asset/js/miarchivo.js
@@ -0,0 +1,2 @@
+let bienvenido = prompt("Bienvenido, ingrese su nombre: ")
+let productos = prompt("Ingrese el producto por el cual esta interesado:  PRODUCTOS ELECTRICOS, TANQUES AEREOS, CAJON DE HERRAMIENTAS" )
